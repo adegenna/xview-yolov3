577ac1fdbdc8c16b6f9fd46093c08f8717635ca6
Anthony DeGennaro <adegennaro@bnl.gov>
2018-12-07 20:43:46 -0500

    Small update to utils/utils

--- utils/utils.py
+++ utils/utils.py
@@ -547,26 +547,6 @@ def pruneTargetFile(nums,mat):
         mat['image_weights'] = np.delete(mat['image_weights'],idx,axis=0)
     return mat;
 
-def get_labels(fname="xView_train.geojson"):
-    # Processes an xView GeoJSON file
-    # INPUT: filepath to the GeoJSON file
-    # OUTPUT: Bounding box coordinate array, Chip-name array, and Class-id array
-    with open(fname) as f:
-        data = json.load(f)
-    coords = np.zeros((len(data['features']),4))
-    chips = np.zeros((len(data['features'])),dtype="object")
-    classes = np.zeros((len(data['features'])))
-    for i in range(len(data['features'])):
-        if data['features'][i]['properties']['bounds_imcoords'] != []:
-            b_id = data['features'][i]['properties']['image_id']
-            val = np.array([int(num) for num in data['features'][i]['properties']['bounds_imcoords'].split(",")])
-            chips[i] = b_id
-            classes[i] = data['features'][i]['properties']['type_id']
-            coords[i] = val
-        else:
-            chips[i] = 'None'
-    return coords, chips, classes
-
 def plot_rgb_image(img,rgb_mean,rgb_std,obj=[]):
     for j in range(3):
         img[:, :, j] *= rgb_std[j]
